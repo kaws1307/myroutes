fef7e5690f9468d6a26bfe1d9d0a3092e5d95fc1
kaws1307 <294895389@qq.com>
2019-12-04 22:06:15 +0800

    change

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="box">
+      <view class="userinfo">
+        <view class="userinfo-avatar">
+          <open-data type="userAvatarUrl"></open-data>
+        </view>
+        <open-data type="userNickName"></open-data>
+      </view>
+    </div>  
   </div>
 </template>
 
@@ -21,4 +29,32 @@ export default {
 </script>
 
 <style scoped>
+.box {
+  width: 90%;
+  height: 200px;
+  background: #666;
+  border-radius: 5px;
+  margin: 5px auto;
+}
+.userinfo {
+  position: relative;
+  width: 750rpx;
+  height: 320rpx;
+  color: #666;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+ 
+.userinfo-avatar {
+  overflow:hidden;
+  display: block;
+  width: 160rpx;
+  height: 160rpx;
+  margin: 20rpx;
+  margin-top: 50rpx;
+  border-radius: 50%;
+  border: 2px solid #fff;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
 </style>
